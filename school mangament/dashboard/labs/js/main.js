74088753aca86b390ae8725fd3a76ba9f3ce25a3
veno32 <govanygozief24@gmail.com>
2024-12-26 11:05:38 +0200

    Delete school mangament/dashboard directory

--- school mangament/dashboard/labs/js/main.js
+++
@@ -1,8 +0,0 @@
-// Add event listeners for "view" buttons
-// document.querySelectorAll('.view-btn').forEach(button => {
-//     button.addEventListener('click', function () {
-//         const labId = this.getAttribute('data-id');
-//         alert(`View details of Lab ${labId}`); // Corrected template literal syntax
-//         // You can navigate to another page or display details in a modal
-//     });
-// });
